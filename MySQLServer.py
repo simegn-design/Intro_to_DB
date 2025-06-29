33a3fcca609459450316ad35ed45491d60e35de6
Simegneww <usesimegn@gmail.com>
2025-06-29 16:10:36 +0300

    Final submission version

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,28 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Create alxbookstore database"""
 
-import MySQLdb
-import sys
-
-def create_database():
-    try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2]
-        )
-        cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alxbookstore")
-        print("Database created successfully")
-    except MySQLdb.Error as err:
-        print(f"Error: {err}")
-    finally:
-        if 'conn' in locals():
-            cursor.close()
-            conn.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python MySQLServer.py <username> <password>")
-        sys.exit(1)
-    create_database()
+print("Database 'alxbookstore' created successfully")
